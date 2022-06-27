9e6a982ead04fa39c0ffe943919b282d7c0d57e9
Andrew J. Otis <104279769+JAMPS657@users.noreply.github.com>
2022-06-27 13:59:18 -0600

    Update Greek Mythology Inspired Text Game.py

--- Personal Programming Projects/Simple Text Adventure Game/Greek Mythology Inspired Text Game.py
+++ Personal Programming Projects/Simple Text Adventure Game/Greek Mythology Inspired Text Game.py
@@ -1,22 +1,3 @@
-# Initial Background:
-# The project is a simulated text-adventure maze game. The theme takes light inspiration from Greek
-# mythology,specifically the Labyrinth and the Minotaur. While not a literal maze, the game attempts
-# to give the player the "sense" of being in one (a very simple one).
-
-# In reference to the Midterm Project rubric on Canvas, the maze is constructed of several functions
-# consisting of multiple variables (local and global). Additionally, there exists control flow blocks
-# (e.g. if statements and for loops), that also call on external functions. Naturally, not every function
-# in the game will have every single aspect previously stated simultaneously, but spread across the code
-# to provide the goal user experience. Strings throughout are manipulated in various ways
-# (e.g. prompts, text for user instructions/experience, being added to a list ,etc.).
-
-# With the goal of adding some variety to user experience, a list is used as data storage to simulate
-# a non-viewable player inventory. Additionally a coin toss sub-game is utilized in the final stage
-# of the game.
-
-#---End of Initial Background---#
-
-
 # The function play_again() asks if user wants play again or not
 def play_again():
     print("\nDo you want to play again? (y or n)")
