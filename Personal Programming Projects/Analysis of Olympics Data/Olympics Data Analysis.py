84067c6b010d188d940ed821f3c0259a515b1be9
Andrew J. Otis <104279769+JAMPS657@users.noreply.github.com>
2022-07-18 16:07:18 -0600

    Update Olympics Data Analysis.py

--- Personal Programming Projects/Analysis of Olympics Data/Olympics Data Analysis.py
+++ Personal Programming Projects/Analysis of Olympics Data/Olympics Data Analysis.py
@@ -1,29 +1,3 @@
-
-#---------------------------------------Initial Background-------------------------------------------------- #
-
-# The concept(s) used in my final project is mainly Data Visualization of a large, pre-scraped dataset of the
-# Olympic Games from 1896 - 2016. There is the reading in of files, but not with File I/O, instead the
-# library pandas is utilized to read in the csv files.
-
-# The goal of the final project is attempt to utilize Python to simply explore, clean, organize, and visualize
-# a large data set (one I would typically use excel or Rstudio to analyze).
-
-# The structure of the project/code is as follows, I will read in and merge two csv files that were provided
-# during the submission of the final project, please save them to your directory. The code will read and merge
-# the csv files. From that point, we will have our raw data set to work with and approach the available data
-# through various perspectives in order to gain some sort of guided understanding.
-
-# Lastly, I have included BOTH coding comments (for those who can view the code) as well as "viewer" comments
-# displayed as the various analysis are run. This serves 2 purposes, the first is to make sense of the project
-# from a coding perspective; but also to make the analysis logical for someone who is just seeing the results
-# of the code being ran. Otherwise, it would appear that the program isn't doing anything at all.
-
-# !Important Caveat!: To have code run as expected, the following must be done
-#    "Ctrl+a" = to select everything THEN "Alt+shift+e" = to run code
-#    The instructions listed above will allow ALL figures to be viewable simultaneously
-
-#----------------------------------End of Initial Background-------------------------------------------------#
-
 # Libraries we'll need to import inorder to perform the analysis/visualization
 import numpy as np
 # numpy is useful for performing up to high level mathematical operations on arrays
